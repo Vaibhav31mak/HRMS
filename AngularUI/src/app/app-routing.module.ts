08d174135c43c35e38e73f11845ee07c47aa08f4
Vaibhav Makwana <121240124+Vaibhav31mak@users.noreply.github.com>
2025-02-17 17:40:29 +0530

    1st iteration with some bugs

---
+++ AngularUI/src/app/app-routing.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './Components/landing/landing.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { OrganizationSettingsComponent } from './Components/organization-settings/organization-settings.component';
+import { NewAdminComponent } from './Components/new-admin/new-admin.component';
+import { AttendanceReportComponent } from './Components/attendance-report/attendance-report.component';
+import { SalaryReportComponent } from './Components/salary-report/salary-report.component';
+import { OfficalDaysComponent } from './Components/offical-days/offical-days.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './Components/update-employee/update-employee.component';
+import { DisplayEmployeeComponent } from './Components/display-employee/display-employee.component';
+import { AddAttendanceComponent } from './Components/add-attendance/add-attendance.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { preventLoginGuard } from './_guards/prevent-login.guard';
+import { RolesComponent } from './Components/roles/roles.component';
+import { UpdateRoleComponent } from './Components/update-role/update-role.component';
+import { AddRoleComponent } from './Components/add-role/add-role.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { UsersComponent } from './Components/users/users.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: SignInComponent,
+    canActivate: [preventLoginGuard],
+  },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'home', component: LandingComponent },
+      {
+        path: 'users',
+        component: UsersComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'users/add',
+        component: NewAdminComponent,
+        canActivate: [AuthGuard],
+      },
+      { path: 'attendance/report', component: AttendanceReportComponent },
+      { path: 'salary/report', component: SalaryReportComponent },
+      { path: 'daysoff', component: OfficalDaysComponent },
+      { path: 'employee/add', component: AddEmployeeComponent },
+      { path: 'settings', component: OrganizationSettingsComponent },
+      { path: 'employee/update', component: UpdateEmployeeComponent },
+      { path: 'employee/display', component: DisplayEmployeeComponent },
+      { path: 'attendance/add', component: AddAttendanceComponent },
+      { path: 'roles', component: RolesComponent },
+      { path: 'roles/update', component: UpdateRoleComponent },
+      { path: 'roles/add', component: AddRoleComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ AngularUI/src/app/app.module.ts
@@ -0,0 +1,93 @@
+// Importing Modules
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+
+// Importing Components
+import { AppComponent } from './app.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { LandingComponent } from './Components/landing/landing.component';
+
+import { NewAdminComponent } from './Components/new-admin/new-admin.component';
+import { AttendanceReportComponent } from './Components/attendance-report/attendance-report.component';
+import { SalaryReportComponent } from './Components/salary-report/salary-report.component';
+import { OfficalDaysComponent } from './Components/offical-days/offical-days.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { DisplayEmployeeComponent } from './Components/display-employee/display-employee.component';
+import { UpdateEmployeeComponent } from './Components/update-employee/update-employee.component';
+import { OrganizationSettingsComponent } from './Components/organization-settings/organization-settings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FilterPipe } from './pipes/filter.pipe';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+import { AddAttendanceComponent } from './Components/add-attendance/add-attendance.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptorInterceptor } from './_interceptors/loading-interceptor.interceptor';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { RolesComponent } from './Components/roles/roles.component';
+import { UpdateRoleComponent } from './Components/update-role/update-role.component';
+import { AddRoleComponent } from './Components/add-role/add-role.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { UsersComponent } from './Components/users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    LandingComponent,
+    NewAdminComponent,
+    AttendanceReportComponent,
+    SalaryReportComponent,
+    OfficalDaysComponent,
+    AddEmployeeComponent,
+    DisplayEmployeeComponent,
+    UpdateEmployeeComponent,
+    OrganizationSettingsComponent,
+    FilterPipe,
+    AddAttendanceComponent,
+    RolesComponent,
+    UpdateRoleComponent,
+    AddRoleComponent,
+    SidebarComponent,
+    UsersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 3000, // Toast disappears after 3 seconds
+      progressBar: true,
+      positionClass: 'toast-bottom-right',
+    }),
+    NgxSpinnerModule.forRoot({
+      type: 'line-scale-party',
+    }),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptorInterceptor,
+      multi: true,
+    },
+  ],
+
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
