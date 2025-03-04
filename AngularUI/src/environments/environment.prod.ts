793453884ebd4ca7d89660a2b04a09e0d60edea2
Vaibhav Makwana <121240124+Vaibhav31mak@users.noreply.github.com>
2025-03-04 21:32:24 +0530

    deployment done

---
+++ AngularUI/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://hrms-app-service-g0frh0djd3bug6gh.centralindia-01.azurewebsites.net/api', // Update with your actual API URL
+  };
+  
